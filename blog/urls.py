0a0147e581093a8f5f1ff7e9b0c3821352d92b2e
thejoker51 <acama23@gmail.com>
2017-01-07 15:22:54 -0500

    Advanced templating

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from . import views
 # post_list is the name of the URL that will identify the view
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
